a61aa5c316831add166dd0b209d9d89c8758e257
Chris Peragine <cperagine@gmail.com>
2018-05-16 11:02:48 -0400

    Animation working as intended

--- liveit12k/liveit12k/ViewController.swift
+++ liveit12k/liveit12k/ViewController.swift
@@ -7,19 +7,28 @@
 //
 
 import UIKit
+import Lottie
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let animationView: LOTAnimationView = LOTAnimationView(name: "liveitani")
+        let screenSize: CGRect = UIScreen.main.bounds
+        print(screenSize.width)
+        
+        animationView.contentMode = .scaleAspectFill
+        animationView.frame = CGRect(x: (screenSize.width/2)-100, y: (screenSize.height/2)-125, width: 200, height: 250)
+        
+        self.view.addSubview(animationView)
+        // Lets turn looping on, since we want it to repeat while the image is 'Downloading'
+        animationView.loopAnimation = true
+        // Now play from 0 to 0.5 progress and loop indefinitely.
+        animationView.play(fromProgress: 0, toProgress: 1, withCompletion: nil)
+        
+        
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
-
