de5c140d2f8ca6be8176a56bc93ba44d37b3c9f3
Chris Peragine <cperagine@gmail.com>
2018-05-23 12:23:21 -0400

    Camera Loads on Page, but nil error after granting permissions

---
+++ liveit12k/liveit12k/UICameraViewController.swift
@@ -0,0 +1,19 @@
+//
+//  UICameraViewController.swift
+//  liveit12k
+//
+//  Created by Chris Peragine on 5/23/18.
+//  Copyright © 2018 South Hills Capstone. All rights reserved.
+//
+
+import Foundation
+import SwiftyCam
+
+class UICameraViewController: SwiftyCamViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+ 
+    }
+    
+}
